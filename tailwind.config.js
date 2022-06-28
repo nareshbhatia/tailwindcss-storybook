33ed19b77a4a9ed599664758c032f076699fdd3c
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-06-27 22:41:21 -0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,7 +54,7 @@ const colors = {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
